0c31d6eb35fddddbe64f6a10c87c18b90c434318
Shanmukh raj <148549871+Demon-sunny@users.noreply.github.com>
2026-05-22 10:30:31 +0530

    Set CORS_ALLOW_ALL_ORIGINS to true for Render frontend compatibility

--- drf_project/drf_project/settings.py
+++ drf_project/drf_project/settings.py
@@ -174,7 +174,7 @@ CSRF_TRUSTED_ORIGINS = [
 #     r"^http://localhost:5173$"
 # ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://lms-frontend-w2ya.onrender.com"
